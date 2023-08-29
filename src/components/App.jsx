081ff88e3aef8b069deaffd549f3986c86eeaeb1
ypernarovskyi@softjourn.com <ypernarovskyi@softjourn.com>
2023-08-29 13:07:06 +0300

    added packeges and Theme folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import { lazy, Suspense, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
-import { useAuth } from "hooks/useAuth";
-import { refreshUser } from "redux/auth/authOperations";
-import { PrivateRoute } from "../privateRoute/PrivateRoute";
-import { RestrictedRoute } from "restrictedRoute/RestrictedRoute";
+import { useAuth } from 'hooks/useAuth';
+import { refreshUser } from 'redux/auth/authOperations';
+import { PrivateRoute } from '../privateRoute/PrivateRoute';
+import { RestrictedRoute } from 'restrictedRoute/RestrictedRoute';
+import { CssVarsProvider } from '@mui/joy';
 
-const HomePage = lazy(() => import("screens/homePage/HomePage"));
-const WelcomePage = lazy(() => import("../screens/welcomePage/WelcomePage"));
-const LoginForm = lazy(() => import("./loginForm/LoginForm"));
-const RegisterForm = lazy(() => import("./registerForm/RegisterForm"));
+import theme from 'components/Theme/Theme';
+
+const HomePage = lazy(() => import('screens/homePage/HomePage'));
+const WelcomePage = lazy(() => import('../screens/welcomePage/WelcomePage'));
+const LoginForm = lazy(() => import('./loginForm/LoginForm'));
+const RegisterForm = lazy(() => import('./registerForm/RegisterForm'));
 const ScrensPage = lazy(() => import('../components/ScreensPage/ScreensPage'));
 
 export const App = () => {
@@ -22,29 +25,67 @@ export const App = () => {
   }, [dispatch]);
 
   return (
-    <Suspense fallback={<b>Загрузка...</b>}>
-      {isRefreshing ? (
-        <b>Refreshing user...</b>
-      ) : (
+    <CssVarsProvider
+      defaultMode="light"
+      disableTransitionOnChange
+      theme={theme}
+    >
+      <Suspense fallback={<b>Загрузка...</b>}>
+        {isRefreshing ? (
+          <b>Refreshing user...</b>
+        ) : (
           <>
             <Toaster />
-        <Routes>
-          <Route index element={<RestrictedRoute component={<WelcomePage />} redirectTo="/home" />} />
-            <Route path="/auth/login"
-              element={<RestrictedRoute component={<LoginForm />} redirectTo="/home" />}
-            />
-            <Route path="/auth/register"
-              element={<RestrictedRoute component={<RegisterForm />} redirectTo="/home" />}
-            />
-            <Route path="/home"
-              element={<PrivateRoute component={<HomePage />} redirectTo="/auth/login" />}
-            />
-            <Route path="/home/:boardName"
-              element={<PrivateRoute component={<ScrensPage />} redirectTo="/auth/login" />}
-            />
-        </Routes>
+            <Routes>
+              <Route
+                index
+                element={
+                  <RestrictedRoute
+                    component={<WelcomePage />}
+                    redirectTo="/home"
+                  />
+                }
+              />
+              <Route
+                path="/auth/login"
+                element={
+                  <RestrictedRoute
+                    component={<LoginForm />}
+                    redirectTo="/home"
+                  />
+                }
+              />
+              <Route
+                path="/auth/register"
+                element={
+                  <RestrictedRoute
+                    component={<RegisterForm />}
+                    redirectTo="/home"
+                  />
+                }
+              />
+              <Route
+                path="/home"
+                element={
+                  <PrivateRoute
+                    component={<HomePage />}
+                    redirectTo="/auth/login"
+                  />
+                }
+              />
+              <Route
+                path="/home/:boardName"
+                element={
+                  <PrivateRoute
+                    component={<ScrensPage />}
+                    redirectTo="/auth/login"
+                  />
+                }
+              />
+            </Routes>
           </>
-      )}
-    </Suspense>
+        )}
+      </Suspense>
+    </CssVarsProvider>
   );
 };
